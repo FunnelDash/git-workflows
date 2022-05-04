b4632b5c3bbbd2ec9152a1398a30cdf32fb41488
Kaleby Cadorin <kaleby@dash.fi>
2022-05-04 17:00:20 -0300

    Added initial files

---
+++ scripts/tag_verify.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -e
+
+#fetch all tags
+echo "[+] Fetching tags"
+git fetch --tags
+
+# get latest tag
+LATEST_TAG=$(git describe --tags `git rev-list --tags --max-count=1`)
+echo "[+] Latest tag is $LATEST_TAG"
+LATEST_TAG_NUMBER=$(git describe --tags `git rev-list --tags --max-count=1` | sed 's/\.//g')
+
+# get the version file tag
+VERSION=$(cat version)
+echo "[+] Current version from version file is: $VERSION"
+VERSION_NUMBER=$(cat version | sed 's/\.//g')
+
+if [ -z "$VERSION" ]
+    then
+        echo "[+] You must set a version to be able proceed."
+        exit 1
+fi
+
+# Setting account
+git config --global user.email "kaleby@dash.fi"
+git config --global user.name "Kaleby Cadorin"
+
+if [ -z "$LATEST_TAG" ]
+    then
+        echo "No tag applied to the repo yet, the tag $VERSION can be applied"
+fi
+
+if [ "$VERSION_NUMBER" -le "$LATEST_TAG_NUMBER" ]
+    then
+        echo "[+] FAIL - The tag version cannot be smaller than $LATEST_TAG"
+        exit 1
+    else
+        echo "[+] SUCCESS - Merge the PR to have tag $VERSION applied to master branch"
+fi
